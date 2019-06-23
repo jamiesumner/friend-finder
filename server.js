38bf8e055b1774348f55acf6db564e7fd335c57e
Jamie Sumner <jamiesumner@Jamies-MacBook-Air.local>
2019-06-22 21:08:31 -0400

    set up connection

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
