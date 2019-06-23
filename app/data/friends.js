e1d822d51730ef08f56369d0fbd340186c1c1899
Jamie Sumner <jamiesumner@Jamies-MacBook-Air.local>
2019-06-22 21:17:57 -0400

    added friends data

---
+++ app/data/friends.js
@@ -0,0 +1,85 @@
+var friends = [
+    {
+        name: "Hannah",
+        photo: "https://www.cheatsheet.com/wp-content/uploads/2019/06/Hannah-Brown-14.jpg",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Becca",
+        photo: "https://media.glamour.com/photos/5b58a5d7ca97003aa0eeb1b4/16:9/w_1280%2Cc_limit/the-bachelorette-becca-kufrin-men-tell-all-2018.jpg",
+        scores: [
+            "3",
+            "4",
+            "2",
+            "1",
+            "5",
+            "2",
+            "2",
+            "1",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "Rachel",
+        photo: "https://pbs.twimg.com/profile_images/1041819181631643649/0zZrDyfu_400x400.jpg",
+        scores: [
+            "3",
+            "5",
+            "2",
+            "1",
+            "2",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Jojo",
+        photo: "https://www.lifeandstylemag.com/wp-content/uploads/2017/10/jojo-fletcher-plastic-surgery.jpg?fit=200%2C1",
+        scores: [
+            "2",
+            "4",
+            "5",
+            "2",
+            "1",
+            "3",
+            "4",
+            "4",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Kaitlyn",
+        photo: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kaitlyn-bristowe-attends-the-broadway-opening-night-after-news-photo-877336060-1557247796.jpg",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "5",
+            "4",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3"
+        ]
+    }
+
+];
+
+module.exports = friends;
